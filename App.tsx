0237f8495b28b57e635190461961e2c651c3e3ea
Rushita <rushitapaghadal@gmail.com>
2025-07-12 11:41:10 +0530

    Integrate deeplinking

--- App.tsx
+++ App.tsx
@@ -6,21 +6,43 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated'; // Mandate to import
 
 // import StackRoute from './src/Route/StackRoute';
 // import TabRoute from './src/Route/TabRoute';
 import DrawerRoute from './src/Route/DrawerRoute';
+import {Linking} from 'react-native';
+import StackRoute from './src/Route/StackRoute';
 
 function App(): React.JSX.Element {
+  const linking = {
+    // enabled: 'auto' /* Automatically generate paths for all screens */,
+    prefixes: ['myapp://', 'https://myapp.com'],
+    config: {
+      screens: {
+        Todo: 'Todo',
+        TodoAdd: 'TodoAdd',
+        NotFound: '*',
+      },
+    },
+  };
+
+  // useEffect(() => {
+  //   const handleDeepLink = ({url}) => {
+  //     console.log('Received deep link:', url);
+  //   };
+
+  //   const sub = Linking.addEventListener('url', handleDeepLink);
+  //   return () => sub.remove();
+  // }, []);
   // console.disableYellowBox ();
 
   return (
-    <NavigationContainer>
-      {/* <StackRoute /> */}
+    <NavigationContainer linking={linking}>
+      <StackRoute />
       {/* <TabRoute /> */}
-      <DrawerRoute />
+      {/* <DrawerRoute /> */}
     </NavigationContainer>
   );
 }
